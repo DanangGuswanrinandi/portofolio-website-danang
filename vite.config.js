b86840bb914feab440d4c224275a69aeb7f340d3
DanangGuswanrinandi <haedardanang@gmail.com>
2025-11-26 15:14:19 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-   base: process.env.NODE_ENV === 'production'
-    ? '/portofolio-website-danang/'  // <-- ini sesuai nama repo mu
-    : '/',
+  base: '/portofolio-website-danang/',
 })
