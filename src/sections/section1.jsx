e1ab35157b604610208d212501084dd3c19c1998
DanangGuswanrinandi <haedardanang@gmail.com>
2025-12-02 12:04:42 +0700

    fix asset path

--- src/sections/section1.jsx
+++ src/sections/section1.jsx
@@ -5,6 +5,7 @@ import { FaWhatsapp, FaInstagram } from "react-icons/fa";
 import { IoCodeSlash } from "react-icons/io5";
 import { SiReact, SiAdobephotoshop, SiCoreldraw } from "react-icons/si";
 import LogoLoop from "../components/LogoLoop";
+import photoshop from "../assets/photoshop.png";
 
 export default function Section1() {
   const techLogos = [
@@ -119,7 +120,7 @@ export default function Section1() {
           <Col md="6" className="hidden md:flex justify-end items-end h-full pb-0 pointer-events-auto">
           {/* Logo Photoshop */}
               <img
-                src="./src/assets/photoshop.png"
+                src={photoshop}
                 className="
                   absolute bottom-85 right-100 w-40 opacity-80 
                   transition-all duration-300
@@ -137,7 +138,7 @@ export default function Section1() {
             
              {/* Logo Coreldraw*/}
               <img
-                src="src/assets/coreldraw.png"
+                src="./src/assets/coreldraw.png"
                 className="
                   absolute top-80 right-20 w-35 opacity-80 
                   transition-all duration-300
@@ -168,7 +169,7 @@ export default function Section1() {
                 "
               />
             <img
-              src="src/assets/Foto Danang.png"
+              src="./src/assets/Foto Danang.png"
               alt="Foto Saya"
               className="rounded-lg w-md-full h-auto max-h-[80vh] object-cover z-10 pointer-events-none"
             />
